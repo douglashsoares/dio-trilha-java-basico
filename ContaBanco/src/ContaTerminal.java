8322b6af126b33966e2891f739aa98e792a04ad7
douglas henrique <33969220+douglashsoares@users.noreply.github.com>
2024-05-30 10:21:56 -0300

    Projeto Implementado e Formatado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String nomeCliente;
+        int numero;
+        String agencia;
+        double saldo = 50.00;
+        String entrada;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Para criar uma conta no Banco precisamos que preencha os seguintes dados");
+        System.out.println("Digite por favor o Nome do Cliente: ");
+        nomeCliente = scanner.next();
+
+        while (true){
+            System.out.println("Digite o seu Número \"Ex:(1213)\": ");
+            entrada = scanner.next();
+
+            if (entrada.matches("\\d{4}")){
+                numero = Integer.parseInt(entrada);
+                break;
+            }else {
+                System.out.println("Digite Somente 4 números");
+            }
+        }
+
+        while (true){
+            System.out.println("Por favor, Digite o número da sua Agência \"Ex:(067-8)\" ");
+            entrada = scanner.next();
+
+            if (entrada.matches("\\d{3}-\\d{1}")){
+                agencia = entrada;
+                break;
+            }else{
+                System.out.println("Formato inválido! Por favor, digite no formato NNN-N, por exemplo, 067-8.");
+            }
+        }
+
+        System.out.println("Conta Criada com Sucesso !!!!");
+
+        System.out.println("----------------------------------------------");
+        System.out.println("Olá " +nomeCliente+ ",\n" +
+                "Obrigado por criar uma conta em nosso banco,\n" +
+                "Sua Agência é: " +agencia+ ",\n" +
+                "Conta Número " +numero+ " e seu Saldo são de " +saldo+ " Reais" +
+                " \nQue estão já estão disponíveis para o saque");
+        System.out.println("----------------------------------------------");
     }
 }
